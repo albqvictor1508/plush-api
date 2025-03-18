e7cb580b7b8bb8f6c1cfac052e40c5f542ed4367
albqvictor1508 <albq.victor@gmail.com>
2025-03-17 21:51:19 -0300

    WIP

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -1,6 +1,7 @@
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "../common/env";
+import postgres from "postgres";
+import { users } from "./schema/users";
 
 export const pg = postgres(env.DATABASE_URL);
 export const db = drizzle(pg, { schema: { users } });
