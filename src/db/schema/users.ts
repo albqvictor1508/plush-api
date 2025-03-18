f81849e836a4eeea6168acae1bf60a887615bff8
albqvictor1508 <albq.victor@gmail.com>
2025-03-17 21:18:18 -0300

    chore: add mock of db schema for users

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,3 +1,10 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {});
+export const users = pgTable("users", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	name: text("name").notNull(),
+	phone: text("phone").notNull().unique(),
+	description: text("description").notNull(),
+	// status: depois eu faço, mei complicado
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
