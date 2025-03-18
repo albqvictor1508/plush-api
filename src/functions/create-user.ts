735968f743a48e119075e9fabdc34dbc55370264
albqvictor1508 <albq.victor@gmail.com>
2025-03-18 08:58:59 -0300

    chore: create user params type in types folder and function created

---
+++ src/functions/create-user.ts
@@ -0,0 +1,3 @@
+import type { CreateUserParams } from "../types/create-user-params";
+
+export async function createUser({ name, phone }: CreateUserParams) {}
---
+++ src/types/create-user-params.ts
@@ -0,0 +1,4 @@
+export type CreateUserParams = {
+	name: string;
+	phone: string;
+};
