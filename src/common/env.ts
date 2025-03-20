d9da78e2152df234b693bd74edae21dd1ca0840d
albqvictor1508 <albq.victor@gmail.com>
2025-03-20 14:11:55 -0300

    WIP: change on environments

--- src/common/env.ts
+++ src/common/env.ts
@@ -3,7 +3,6 @@ import { cleanEnv, num, str, url } from "envalid";
 export const env = cleanEnv(process.env, {
 	DATABASE_URL: url(),
 	JWT_SECRET: str(),
-	MY_PHONE: str(),
 	VONAGE_API_KEY: str(),
 	VONAGE_API_SECRET: str(),
 	PORT: num(),
