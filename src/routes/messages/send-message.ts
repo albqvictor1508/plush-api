4cc5338df3cec9b2028780cf454db1c7434822ab
albqvictor1508 <albq.victor@gmail.com>
2025-03-21 08:07:23 -0300

    chore: websocket in api v:0.0.1

--- src/routes/messages/send-message.ts
+++ src/routes/messages/send-message.ts
@@ -1,15 +1,16 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import type WebSocket from "ws";
 
-const clients = new Set(); //criar um conjunto pra guardar as conexões
+const clients = new Set<WebSocket>(); //criar um conjunto pra guardar as conexões
 
 export const sendMessageRoute: FastifyPluginAsyncZod = async (app) => {
 	app.get("/api/messages/send", { websocket: true }, (socket, req) => {
 		clients.add(socket);
-		socket.on("message", (message) => {
+		socket.on("message", (data) => {
 			for (const client of clients) {
 				//ver se não sou eu e pegar todos que estão com a comunicação aberta (conectados)
-				if (client !== socket && socket.readyState === socket.OPEN) {
-					client.send();
+				if (client !== socket && client.readyState === client.OPEN) {
+					client.send(data);
 				}
 			}
 		});
