9e73e081fd0f9ea3d8fb44f435d1b0fa5e283a74
albqvictor1508 <albq.victor@gmail.com>
2025-03-25 09:56:38 -0300

    perf]: changes on new user route to more performance

--- src/functions/send-code-to-user.ts
+++ src/functions/send-code-to-user.ts
@@ -1,4 +1,5 @@
 import type { CreateUserParams, NewAccountTemporaryData } from "../types/auth";
+import { handleSendEmail } from "../utils/send-email";
 
 export const codes = {} as Record<string, NewAccountTemporaryData>;
 const ONE_SECOND_IN_MS = 1000;
@@ -14,24 +15,25 @@ setInterval(() => {
 
 export async function sendCodeToUser({ name, email }: CreateUserParams) {
 	const generatedCode = Math.random().toString().slice(2, 6);
-	//"83991303948": {name: "victor", code: "1649", phone: "83991303948", generatedAt: um numero imenso}
 	try {
-
-		const emailSubject = 'Seu código de verificação';
-   		const emailText = `Bom dia, ${name}! Seu código de verificação é: ${generatedCode}`;
-		
-		//depois eu analiso se tem possibilidade de rodar isso em paralelo pra ganho de performance
-		await handleSendEmail({ subject: emailSubject,
-			email,
-			text: emailText,
-			message: emailText, });
-    
-			codes[email] = {
-			name,
-			code: generatedCode,
-			email,
-			generatedAt: Date.now(),
-		};
+		const emailSubject = "Seu código de verificação";
+		const emailText = `Bom dia, ${name}! Seu código de verificação é: ${generatedCode}`;
+		await Promise.all([
+			await handleSendEmail({
+				subject: emailSubject,
+				email,
+				text: emailText,
+				message: emailText,
+			}),
+			// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+			(codes[email] = {
+				name,
+				code: generatedCode,
+				email,
+				generatedAt: Date.now(),
+			}),
+			//"myemail@gmail.com": {name: "victor", code: "1649", email: "myemail@gmail.com", generatedAt: um numero imenso}
+		]);
 	} catch (e) {
 		console.log(e);
 	}
