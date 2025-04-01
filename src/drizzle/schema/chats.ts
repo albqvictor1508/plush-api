5f7091dacad9c371fa7f8a549fdd210947c7f752
albqvictor1508 <albq.victor@gmail.com>
2025-04-01 12:38:47 -0300

    test

--- src/drizzle/schema/chats.ts
+++ src/drizzle/schema/chats.ts
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
 import { pgTable, serial, text, uuid, timestamp } from "drizzle-orm/pg-core";
-=======
-import {
-	pgTable,
-	serial,
-	text,
-	boolean,
-	uuid,
-	timestamp,
-} from "drizzle-orm/pg-core";
->>>>>>> f14857a (chore: changes on schema to make the chats)
 import { users } from "./users";
 
 export const chats = pgTable("chats", {
 	id: serial("id").notNull().primaryKey(),
 	title: text("title"),
-<<<<<<< HEAD
 	createdBy: uuid("created_by")
 		.notNull()
 		.references(() => users.id, {
 			onDelete: "cascade",
 		}),
-	lastMessageAt: timestamp("last_message_at").defaultNow().notNull(),
+	lastMessageAt: timestamp("last_message_at").defaultNow(),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
-=======
-	isGroup: boolean("is_group").default(false).notNull(),
-	createdAt: uuid("created_by").references(() => users.id),
-	lastMessageAt: timestamp("last_message_at"),
->>>>>>> f14857a (chore: changes on schema to make the chats)
 });
