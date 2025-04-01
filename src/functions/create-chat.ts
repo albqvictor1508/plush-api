07273c643c1d313b343e8da9099b3d52284b38f8
albqvictor1508 <albq.victor@gmail.com>
2025-03-31 21:28:33 -0300

    test

---
+++ src/functions/create-chat.ts
@@ -0,0 +1,32 @@
+import { db } from "../drizzle/client";
+import { chatParticipants } from "../drizzle/schema/chat-participants";
+import { chats } from "../drizzle/schema/chats";
+import { Type, type CreateChatParams } from "../types/messages";
+
+export async function createChat({
+	title,
+	ownerId,
+	participantId,
+}: CreateChatParams) {
+	let type: Type;
+	const participants = await db.$count(chatParticipants.userId);
+	if (participants > 2) {
+		type = Type.GROUP;
+		return;
+	}
+	//cria chat
+	const [chat] = await db
+		.insert(chats)
+		.values({ type: "private", title, createdBy: ownerId })
+		.returning();
+
+	await db
+		.insert(chatParticipants)
+		.values({ userId: ownerId, chatId: chat.id });
+
+	await db
+		.insert(chatParticipants)
+		.values({ userId: participantId, chatId: chat.id });
+
+	return chat;
+}
