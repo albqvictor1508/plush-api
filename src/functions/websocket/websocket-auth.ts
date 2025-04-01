c941dca049e25ad22e54f694aa56a441955c2ac7
albqvictor1508 <albq.victor@gmail.com>
2025-04-01 09:39:23 -0300

    chore: add parse cookie function on utils

--- src/functions/websocket/websocket-auth.ts
+++ src/functions/websocket/websocket-auth.ts
@@ -1,28 +1,17 @@
 import type { FastifyInstance } from "fastify";
 import type { IncomingMessage } from "node:http";
 import type { JWTDecoded } from "../../types/auth";
+import { parseCookie } from "../../utils/parse-cookie";
 
 export async function websocketAuth(
 	app: FastifyInstance,
 	info: { req: IncomingMessage },
 ): Promise<JWTDecoded | null> {
 	try {
-		const cookieHeader = info.req.headers.cookie || ""; //pega o cookie se tiver o cookie
-		const cookies = app.parseCookie(cookieHeader); //parseia o cookie pra um objeto
+		const cookieHeader = info.req.headers.cookie || "";
+		const user = await parseCookie(cookieHeader);
 
-		if (!cookies?.plush_auth) {
-			throw new Error("Missing auth cookie");
-		}
-
-		const token = decodeURIComponent(cookies.plush_auth); //tira os caracter especial do token
-
-		const decoded = app.jwt.verify<JWTDecoded>(token); //verifica se o cookie é válido
-
-		if (!decoded?.id || !decoded?.email) {
-			throw new Error("Invalid JWT payload!");
-		}
-
-		return { id: decoded.id, email: decoded.email };
+		return user;
 	} catch (e) {
 		app.log.error(`Failed to authenticate on Websocket: ${e}`);
 		return null;
--- src/routes/chats/create-chat-route.ts
+++ src/routes/chats/create-chat-route.ts
@@ -1,20 +1,22 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { createChat } from "../../functions/create-chat";
+import { app } from "../../server";
 
-export const createChatRoute: FastifyPluginAsyncZod = async (app) => {
-	app.post(
+export const createChatRoute: FastifyPluginAsyncZod = async (fastify) => {
+	fastify.post(
 		"/api/chats",
 		{
 			schema: {
+				headers: z.object({
+					cookie: z.string(),
+				}),
 				body: z.object({
 					title: z.string(),
-					ownerId: z.string(),
 					participantId: z.string(),
 				}),
 				response: {
 					201: z.object({
-						type: z.string(),
 						id: z.number(),
 						createdAt: z.date(),
 						createdBy: z.string().uuid(),
@@ -24,8 +26,11 @@ export const createChatRoute: FastifyPluginAsyncZod = async (app) => {
 			},
 		},
 		async (request, reply) => {
-			const { title, ownerId, participantId } = request.body;
-			const chat = await createChat({ title, ownerId, participantId });
+			const { userId } = app.parseCookie(request.headers.cookie);
+			console.log(userId);
+
+			const { title, participantId } = request.body;
+			const chat = await createChat({ title, ownerId: userId, participantId });
 			return reply.status(201).send(chat);
 		},
 	);
