389fa1cdd1affe5f2e30377953e1560510f53b1c
albqvictor1508 <albq.victor@gmail.com>
2025-04-03 20:52:28 -0300

    wip: i think its completed, missing tests

--- src/functions/list-chats-by-user.ts
+++ src/functions/list-chats-by-user.ts
@@ -1,12 +1,18 @@
-import { eq } from "drizzle-orm";
+import { eq, max, or } from "drizzle-orm";
 import { db } from "../drizzle/client";
-import { chats, users } from "../drizzle/schema";
+import { chatParticipants, chats, messages, users } from "../drizzle/schema";
 
 export async function listChatsByUser(userId: string) {
-	const [listedChats] = await db
+	const listedChats = await db
 		.select()
 		.from(chats)
-		.where(eq(users.id, userId));
+		.where(eq(users.id, userId))
+		.orderBy(chats.lastMessageAt);
 
-	return { chats: listedChats };
+	const lastMessage = await db
+		.select({ value: max(messages.createdAt) })
+		.from(messages)
+		.where(eq(chatParticipants.userId, userId));
+
+	return { chats: { ...listedChats, lastMessage } };
 }
--- src/routes/chats/list-chats-by-user-route.ts
+++ src/routes/chats/list-chats-by-user-route.ts
@@ -1,9 +1,11 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { parseCookie } from "../../utils/parse-cookie";
+import { listChatsByUser } from "../../functions/list-chats-by-user";
 
 export const listChatsByUserRoute: FastifyPluginAsyncZod = async (app) => {
 	app.get("/api/chats", {}, async (request, reply) => {
 		const { id } = await parseCookie(request.headers.cookie || "");
-		const { chats } = listChatsByUser(id);
+		const { chats } = await listChatsByUser(id);
+		return chats;
 	});
 };
