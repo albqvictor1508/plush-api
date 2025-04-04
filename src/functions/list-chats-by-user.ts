695853e29ba88d155e72830c390994ff1fc0622d
albqvictor1508 <albq.victor@gmail.com>
2025-04-04 15:15:52 -0300

    feat: feature completed

--- src/functions/list-chats-by-user.ts
+++ src/functions/list-chats-by-user.ts
@@ -1,23 +1,12 @@
-import { and, desc, eq, max} from "drizzle-orm";
+import { desc, eq, isNotNull, max} from "drizzle-orm";
 import { db } from "../drizzle/client";
 import { chats, messages } from "../drizzle/schema";
 
 export async function listChatsByUser(userId: string) {
-  // pegar o maior id entre as mensagens (que seria a mais recente, pq o id é incremental)
-  //agrupo por chat id, ai pego o maior id daquele grupo
-  //tenho que validar isso também
-  const [lastMessageId] = await db.select({chatId: messages.chatId,messageId: max(messages.id)}).from(messages).groupBy(messages.chatId);
-  console.log(`THE BIGGER MESSAGE ID IS ${lastMessageId.messageId}, CONTENT: por enquanto vazio`);
-  //pegar a mensagem
+  const listedChats = await db
+  .select({title: chats.title, id: chats.id, lastMessage: messages.content, lastMessageAt: chats.lastMessageAt}).from(chats).where(isNotNull(chats.lastMessageAt))
+  .innerJoin(messages, eq(messages.chatId, chats.id))
+  .orderBy(desc(chats.lastMessageAt))
 
-  //essa msr ta retornando todas as mensagens do chat que foi enviada a mensagem com maior id
-	const listedChats = await db
-		.select({title: chats.title, chatId: chats.id, lastMessageAt: chats.lastMessageAt, lastMessage: messages.content})
-		.from(chats)
-    .innerJoin(messages, and(eq(chats.id, lastMessageId.chatId), eq(messages.id, lastMessageId.messageId as number))) //erro ta aqui
-		.orderBy(desc(chats.lastMessageAt));
-
-	return { chat: {listedChat: listedChats.map(chat => {chat})} };
-
-  //return {chat: listedChats}
+  return {chat: listedChats}
 }
