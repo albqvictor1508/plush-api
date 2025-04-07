1f68abc079cc7430fb058eb715abf6145de3c2e0
albqvictor1508 <albq.victor@gmail.com>
2025-04-07 10:41:53 -0300

    chore: changes on codebase and particion feature completed

--- src/functions/update-get-file.ts
+++ src/functions/update-get-file.ts
@@ -2,16 +2,25 @@ import { s3 } from "../common/storage/r2-config";
 import { env } from "../common/env";
 import type { UploadParams } from "../types/images";
 
+const setFileUrl = (userId: string, photoType: string, fileName: string) => {
+	return {
+		fullPath: `https://${env.R2_BUCKET_NAME}.${env.R2_ENDPOINT}/${userId}/${photoType}/${fileName}`,
+	};
+};
+
 export const uploadFile = async ({
 	userId,
-	type,
+	photoType,
 	fileName,
 	fileContent,
 }: UploadParams): Promise<string> => {
+	const uniqueFileName = `${Date.now()}-${fileName}`;
+	const { fullPath } = setFileUrl(userId, photoType, uniqueFileName);
+
 	try {
 		const params = {
 			Bucket: env.R2_BUCKET_NAME,
-			Key: fileName,
+			Key: fullPath,
 			Body: fileContent,
 			ACL: "private",
 		};
@@ -24,8 +33,7 @@ export const uploadFile = async ({
 	}
 };
 
-// Função para gerar URL do arquivo armazenado
-export const getFileUrl = (fileName: string): string => {
-	//adicionar o type, o userId, aí sim o filename
-	return `https://${env.R2_BUCKET_NAME}.${env.R2_ENDPOINT}/${fileName}`;
+export const getFileUrl = ({ fileName, photoType, userId }): string => {
+	const { fullPath } = setFileUrl(fileName, photoType, userId);
+	return fullPath;
 };
