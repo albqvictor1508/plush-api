82024fd878733da005c73450131849215cd6509a
albqvictor1508 <albq.victor@gmail.com>
2025-04-08 10:54:19 -0300

    feat: profile completed

--- src/routes/profile/@me.ts
+++ src/routes/profile/@me.ts
@@ -1,5 +1,4 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import { z } from "zod";
 import { PhotoType } from "../../types/images";
 import { uploadFile } from "../../functions/images/upload-file";
 import { parseCookie } from "../../utils/parse-cookie";
@@ -9,7 +8,7 @@ import { users } from "../../drizzle/schema";
 import { eq } from "drizzle-orm";
 
 export const getProfileRoute: FastifyPluginAsyncZod = async (app) => {
-	app.put("/api/@me", {}, async (request, reply) => {
+	app.put("/api/@me", async (request, reply) => {
 		const { id: userId } = await parseCookie(request.headers.cookie || "");
 		const data = await request.file();
 		if (!data) {
@@ -29,13 +28,13 @@ export const getProfileRoute: FastifyPluginAsyncZod = async (app) => {
 		} catch (error) {
 			throw new Error(chalk.bgGreen(`ERROR ON PARSE DATA: ${error}`));
 		}
+
 		const [user] = await db
 			.select({ name: users.name, email: users.email })
 			.from(users)
 			.where(eq(users.id, userId));
 
 		try {
-			//por agora aceitar qualquer merda, mas dps quero filtrar por mimetype
 			const fileBuffer = await data?.toBuffer();
 
 			const fileUrl = await uploadFile({
@@ -45,7 +44,7 @@ export const getProfileRoute: FastifyPluginAsyncZod = async (app) => {
 				photoType: PhotoType.PROFILE,
 			});
 
-			return reply.status(200).send({ user, photo: fileUrl });
+			return reply.status(200).send({ user, fileUrl });
 		} catch (error) {
 			throw new Error(chalk.bgCyan(`ERROR TO ADD IMAGE ON STORAGE: ${error}`));
 		}
