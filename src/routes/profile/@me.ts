0a870e3bd8dc033a9b8e58c52807505a55a01ae4
albqvictor1508 <albq.victor@gmail.com>
2025-04-08 16:17:35 -0300

    chore: great progress on update user route

--- src/routes/profile/@me.ts
+++ src/routes/profile/@me.ts
@@ -1,6 +1,9 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { PhotoType } from "../../types/images";
-import { uploadFile } from "../../functions/images/upload-file";
+import {
+	checkFileExists,
+	uploadFile,
+} from "../../functions/images/upload-file";
 import { parseCookie } from "../../utils/parse-cookie";
 import chalk from "chalk";
 import { db } from "../../drizzle/client";
@@ -9,39 +12,27 @@ import { eq } from "drizzle-orm";
 import { getFileUrl } from "../../functions/images/file-url";
 
 export const getProfileRoute: FastifyPluginAsyncZod = async (app) => {
-	app.put("/api/@me", async (request, reply) => {
+	app.get("/api/@me", async (request, reply) => {
 		const { id: userId } = await parseCookie(request.headers.cookie || "");
-		const data = await request.file();
-		if (!data) {
-			const [user] = await db
-				.select({ name: users.name, email: users.email })
-				.from(users)
-				.where(eq(users.id, userId));
-
-			return reply.status(200).send(user);
-		}
-		const { profileData } = data.fields;
-		let parsedData: { name: string; email: string };
-
-		try {
-			parsedData = JSON.parse(profileData?.value);
-			const { name, email } = parsedData;
-		} catch (error) {
-			throw new Error(chalk.bgGreen(`ERROR ON PARSE DATA: ${error}`));
-		}
 
 		const [user] = await db
 			.select({ name: users.name, email: users.email })
 			.from(users)
 			.where(eq(users.id, userId));
 
+		let fileUrl: string | null = null;
+		const fileName = "profile-photo";
+		const fileExists = await checkFileExists(
+			`${userId}/${PhotoType.PROFILE}/${fileName}`,
+		);
 		try {
-			const fileName = data.filename || "profile-photo";
-			const fileUrl = await getFileUrl({
-				fileName,
-				photoType: PhotoType.PROFILE,
-				userId,
-			});
+			if (fileExists) {
+				fileUrl = await getFileUrl({
+					fileName,
+					photoType: PhotoType.PROFILE,
+					userId,
+				});
+			}
 
 			return reply.status(200).send({ user, fileUrl });
 		} catch (error) {
--- src/routes/update-user.ts
+++ src/routes/update-user.ts
@@ -1,28 +1,48 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { parseCookie } from "../utils/parse-cookie";
+import { db } from "../drizzle/client";
+import { users } from "../drizzle/schema";
+import { uploadFile } from "../functions/images/upload-file";
+import { eq } from "drizzle-orm";
+import chalk from "chalk";
+import { z } from "zod";
+
+const updateUserSchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+});
 
 export const updateUserRoute: FastifyPluginAsyncZod = async (app) => {
 	app.put("/api/user", async (request, reply) => {
 		const { id: userId } = await parseCookie(request.headers.cookie || "");
 		const data = await request.file();
+		const { profileData } = data?.fields;
+		let parsedData: { name: string; email: string };
+
 		if (!data) {
-			const [user] = await db
-				.select({ name: users.name, email: users.email })
-				.from(users)
-				.where(eq(users.id, userId));
+			const parsedData: { name: string; email: string } = JSON.parse(
+				profileData.value,
+			);
+			updateUserSchema.parse(parsedData);
+			const { name, email } = parsedData;
 
-			await uploadFile({
-				userId,
-				fileName,
-				fileContent: fileBuffer,
-				photoType: PhotoType.PROFILE,
-			});
-
-			const fileBuffer = await data?.toBuffer();
+			await db; //update no user
 
 			return reply.status(200).send(user);
 		}
-		const { profileData } = data.fields;
-		let parsedData: { name: string; email: string };
+
+		try {
+			const salve = "salve";
+		} catch (error) {
+			throw new Error(chalk.bgCyanBright(`ERROR: ${error}`));
+		}
+
+		const fileBuffer = await data?.toBuffer();
+		await uploadFile({
+			userId,
+			fileName: "profile-photo",
+			fileContent: fileBuffer,
+			photoType: PhotoType.PROFILE,
+		});
 	});
 };
