f700e8fa0b62d80a4d5e841b35b40ec8be3eac04
albqvictor1508 <albq.victor@gmail.com>
2025-05-23 09:49:56 -0300

    wip: validation completed, now missing refactor

--- src/routes/messages/delete-message.ts
+++ src/routes/messages/delete-message.ts
@@ -3,28 +3,38 @@ import { z } from "zod";
 import { parseCookie } from "../../utils/parse-cookie";
 import { db } from "../../drizzle/client";
 import { chatParticipants, users } from "../../drizzle/schema";
-import { eq, and} from "drizzle-orm";
-
+import { eq, and } from "drizzle-orm";
 
 export const deleteMessageRoute: FastifyPluginAsyncZod = async (app) => {
 	app.delete(
 		"/api/messages",
 		{
 			schema: {
-				body: z.object({ userId: z.string(), chatId: z.number(), messageId: z.number() }),
+				body: z.object({
+					userId: z.string(),
+					chatId: z.number(),
+					messageId: z.number(),
+				}),
 			},
 		},
-		async (request, response) => {
-            const {userId,chatId,messageId} = request.body;
+		async (request, reply) => {
+			const { userId, chatId, messageId } = request.body;
 
-            const [user] = await db
-            .select({id: users.id,role: chatParticipants.role })
-            .from(chatParticipants)
-            .where(
-                and(eq(chatParticipants.chatId, chatId), eq(chatParticipants.userId, userId))
-            );
+			const [user] = await db
+				.select({ id: users.id, role: chatParticipants.role })
+				.from(chatParticipants)
+				.where(
+					and(
+						eq(chatParticipants.chatId, chatId),
+						eq(chatParticipants.userId, userId),
+					),
+				);
+			if (!user) return reply.status(404).send("user not founded");
 			const { id } = await parseCookie(request.headers.cookie || "");
-            if(user.id !== id || user.role !== "admin")
+			if (user.id !== id || user.role !== "admin")
+				return reply
+					.status(400)
+					.send("the message has to be yours or you have to be an admin");
 		},
 	);
 };
