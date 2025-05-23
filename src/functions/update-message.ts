11a9972cb12c683731237957f198c6dc41804ceb
albqvictor1508 <albq.victor@gmail.com>
2025-05-23 09:32:44 -0300

    fix: fix query completed

--- src/functions/update-message.ts
+++ src/functions/update-message.ts
@@ -3,32 +3,14 @@ import { messages, users } from "../drizzle/schema";
 import { and, eq } from "drizzle-orm";
 
 export async function updateMessage(
-	userIdParam: string,
-	messageIdParam: number,
-	contentParam: string,
+	userId: string,
+	messageId: number,
+	content: string,
 ) {
-	const allData = db
-		.select({
-			messageId: messages.id,
-			messageUserId: messages.userId,
-			userId: users.id,
-			updatedAt: messages.updatedAt,
-		})
-		.from(messages)
-		.innerJoin(users, eq(users.id, messages.userId))
-		.as("message_user");
-
 	const updatedMessage = await db
-		.with(allData)
 		.update(messages)
-		.set({ content: contentParam, updatedAt: new Date() })
-		.where(
-			and(
-				eq(allData.userId, userIdParam),
-				eq(allData.messageId, messageIdParam),
-			),
-		)
+		.set({ content, updatedAt: new Date() })
+		.where(and(eq(messages.userId, userId), eq(messages.id, messageId)))
 		.returning();
-
 	return updatedMessage;
 }
