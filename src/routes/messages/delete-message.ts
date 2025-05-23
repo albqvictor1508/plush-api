625e1b3ea7eba0590b0e26841468e879f82309ff
albqvictor1508 <albq.victor@gmail.com>
2025-05-23 09:46:49 -0300

    wip: query completed, missing validation

--- src/routes/messages/delete-message.ts
+++ src/routes/messages/delete-message.ts
@@ -1,17 +1,30 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { parseCookie } from "../../utils/parse-cookie";
+import { db } from "../../drizzle/client";
+import { chatParticipants, users } from "../../drizzle/schema";
+import { eq, and} from "drizzle-orm";
+
 
 export const deleteMessageRoute: FastifyPluginAsyncZod = async (app) => {
 	app.delete(
-		"/api/messages/:userId",
+		"/api/messages",
 		{
 			schema: {
-				params: z.object({ userId: z.string() }),
+				body: z.object({ userId: z.string(), chatId: z.number(), messageId: z.number() }),
 			},
 		},
 		async (request, response) => {
+            const {userId,chatId,messageId} = request.body;
+
+            const [user] = await db
+            .select({id: users.id,role: chatParticipants.role })
+            .from(chatParticipants)
+            .where(
+                and(eq(chatParticipants.chatId, chatId), eq(chatParticipants.userId, userId))
+            );
 			const { id } = await parseCookie(request.headers.cookie || "");
+            if(user.id !== id || user.role !== "admin")
 		},
 	);
 };
