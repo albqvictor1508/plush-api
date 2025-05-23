4fd01e238bee9b5d288c123c891d0328b54d6c2a
albqvictor1508 <albq.victor@gmail.com>
2025-05-23 11:42:38 -0300

    fix: add description and change multipart func

--- src/routes/upload-image.ts
+++ src/routes/upload-image.ts
@@ -2,19 +2,26 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { uploadFile } from "../functions/images/upload-file";
 import { parseCookie } from "../utils/parse-cookie";
 import { PhotoType } from "../types/images";
+import { z } from "zod";
 
 export const uploadImage: FastifyPluginAsyncZod = async (app) => {
-	app.post("/api/images", async (request, reply) => {
-		const { id } = await parseCookie(request.headers.cookie || "");
-		const files = request.files();
-		for await (const file of files) {
-			const fileBuffered = await file.toBuffer();
-			await uploadFile({
-				userId: id,
-				photoType: PhotoType.IMAGE,
-				fileName: file.filename,
-				fileContent: fileBuffered,
-			});
-		}
-	});
+	app.post(
+		"/api/images",
+		{ schema: { body: z.object({ description: z.nullable(z.string()) }) } },
+		async (request, reply) => {
+			const { id } = await parseCookie(request.headers.cookie || "");
+			const parts = request.parts();
+			for await (const part of parts) {
+				if (part.type === "file") {
+					const fileBuffered = await part.toBuffer();
+					await uploadFile({
+						userId: id,
+						photoType: PhotoType.IMAGE,
+						fileName: part.filename,
+						fileContent: fileBuffered,
+					});
+				}
+			}
+		},
+	);
 };
