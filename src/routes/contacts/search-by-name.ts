058ef34ebb1dbeb4c96d6f0e1b808eeb3ab79ffb
albqvictor1508 <albq.victor@gmail.com>
2025-05-24 19:58:29 -0300

    wip: search name route completed

--- src/routes/contacts/search-by-name.ts
+++ src/routes/contacts/search-by-name.ts
@@ -1,12 +1,15 @@
 import {FastifyPluginAsyncZod} from "fastify-type-provider-zod"
 import {z} from "zod";
+import { SearchContactByName } from "../../functions/contacts/search-by-name";
 
 export const SearchContactByNameRoute: FastifyPluginAsyncZod = async(app) => {
     app.get("/api/contacts/:name", {schema: {
-        params: {
+        params: z.object({
             name: z.string()
-        }
+        })
     }}, async (request, reply) => {
-        
+        const {name} = request.params;
+        const usersList = await SearchContactByName(name);
+        return reply.status(200).send(usersList);
     })
 }
