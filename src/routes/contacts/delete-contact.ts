5c32c836c6526018da5f8dca5736059ec9c8d835
albqvictor1508 <albq.victor@gmail.com>
2025-05-24 15:14:39 -0300

    wip: delete contact route in progress

--- src/routes/contacts/delete-contact.ts
+++ src/routes/contacts/delete-contact.ts
@@ -1,1 +1,17 @@
-export const DeleteContactRoute
+import {FastifyTypeProviderZod} from "fastify-type-provider-zod"
+import {z} from "zod";
+
+export const DeleteContactRoute: FastifyTypeProviderZod async (app) => {
+  app.delete("api/contacts", {
+    schema: {
+      body: z.object({
+        email: z.string().email()
+      })
+    }
+  }, async (request, reply) => {
+    const {id: userId} = await parseCookie(request.headers.cookie || "");
+    const {email} = request.body;
+
+    return reply.status(200);
+  }) 
+}
