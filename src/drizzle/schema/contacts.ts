5d38d72659f78390ea2dc7a83b77856eb6839d19
albqvictor1508 <albq.victor@gmail.com>
2025-05-24 14:07:49 -0300

    contact schema created

--- src/drizzle/schema/contacts.ts
+++ src/drizzle/schema/contacts.ts
@@ -1,7 +1,17 @@
-import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	boolean,
+	integer,
+	pgTable,
+	serial,
+	text,
+	timestamp,
+} from "drizzle-orm/pg-core";
 
 export const contacts = pgTable("contacts", {
 	id: serial("id").primaryKey().notNull(),
-	name: text("name").notNull().unique(),
-	email: text(),
+	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
+	isFixed: boolean("is_fixed").$default(() => false),
+	createdAt: timestamp("created_at").defaultNow(),
+	updatedAt: timestamp("updated_at"),
 });
