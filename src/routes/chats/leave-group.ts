18b697839156e33469b550dce276c67e3ada14c1
albqvictor1508 <albq.victor@gmail.com>
2025-05-25 10:28:27 -0300

    wip: leave group route query to verify the group exists

--- src/routes/chats/leave-group.ts
+++ src/routes/chats/leave-group.ts
@@ -1,5 +1,8 @@
 import {FastifyPluginAsyncZod} from "fastify-type-provider-zod"
 import {z} from "zod"
+import { db } from "../../drizzle/client";
+import { and, eq, exists } from "drizzle-orm";
+import { chatParticipants, chats } from "../../drizzle/schema";
 
 export const LeaveGroupRoute: FastifyPluginAsyncZod = async(app) => {
     app.delete("/api/groups/:chatId/leave/:participantId", {
@@ -10,6 +13,13 @@ export const LeaveGroupRoute: FastifyPluginAsyncZod = async(app) => {
             })
         }
     },async (request, reply) => {
+        const {chatId,participantId} = request.params;
+        const [groupExists] = await db
+        .select({exists: exists(chatParticipants)})
+        .from(chatParticipants)
+        .innerJoin(chats, eq(chats.id, chatParticipants.chatId))
+        .where(and(eq(chatParticipants.chatId, chatId), eq(chatParticipants.userId, participantId), eq(chats.chatType, "group")));
 
+        if(!groupExists) return reply.status(404).send("This group not exists");
     })
 }
