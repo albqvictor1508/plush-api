70bd7bb5c4ab1afa7adaaf6da00849cbe39734ac
albqvictor1508 <albq.victor@gmail.com>
2025-05-25 12:44:50 -0300

    wip: create group route participants validation completed

--- src/routes/chats/create-group.ts
+++ src/routes/chats/create-group.ts
@@ -1,5 +1,9 @@
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import {z} from "zod";
+import { createChat } from "../../functions/create-chat";
+import { db } from "../../drizzle/client";
+import { eq, exists } from "drizzle-orm";
+import { users } from "../../drizzle/schema";
 
 export const CreateGroupRoute: FastifyPluginAsyncZod = async(app) => {
     app.post("/api/groups", {
@@ -12,6 +16,15 @@ export const CreateGroupRoute: FastifyPluginAsyncZod = async(app) => {
             })
         }
     },async (request, reply) => {
+        const {title,createdBy,participantsId,photoUrl} = request.body;
+        for(const participantId of participantsId) {
+            const [participantExists] = await db
+            .select({exists: exists(users)})
+            .from(users)
+            .where(eq(users.id, participantId));
 
+            if(!participantExists) return reply.status(400).send(`the user with id: ${participantId} not exists`);
+        }
+        const group = createChat({title, participantsId, ownerId: createdBy});
     })
 }
