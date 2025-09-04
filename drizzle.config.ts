b8029195a50d9ff45a166d6d5281bfaea48e7d8c
albqvictor1508 <albq.victor@gmail.com>
2025-09-04 20:22:32 -0300

    feat: add Drizzle ORM config for PostgreSQL with schema and DB credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,1 +1,12 @@
-import { } from "drizzle-orm"
+import { defineConfig } from "drizzle-kit";
+import { env } from "./src/common/env";
+
+export default defineConfig({
+  schema: "./src/db/schema/**.ts",
+  out: "./src/db/migrations",
+  dialect: "postgresql",
+  casing: "snake_case",
+  dbCredentials: {
+    url: env.DATABASE_URL
+  }
+})
