7f88c1aab0c69ca0656687c5d6bc4deb4a33cfa1
albqvictor1508 <albq.victor@gmail.com>
2025-09-05 09:22:02 -0300

    feat(auth): add JWT authentication and cookie support with new dependencies

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,6 +1,7 @@
 import * as e from "envalid"
 
 export const env = e.cleanEnv(process.env, {
+  JWT_SECRET: e.str(),
   NODE_ENV: e.str(),
   DATABASE_URL: e.url(),
   REDIS_HOST: e.str(),
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,8 @@
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { fastify } from "fastify"
 import { type ZodTypeProvider, validatorCompiler, serializerCompiler } from "fastify-type-provider-zod"
+import { env } from "src/common/env";
 
 export async function createApp() {
   const app = fastify()
@@ -7,6 +10,19 @@ export async function createApp() {
   app.withTypeProvider<ZodTypeProvider>();
   app.setValidatorCompiler(validatorCompiler)
   app.setSerializerCompiler(serializerCompiler)
+
+  app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
+  })
+
+  app.register(fastifyCookie, {
+    secret: env.JWT_SECRET,
+    parseOptions: {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: env.NODE_ENV === "prod"
+    }
+  })
 
   const ALLOWED_HEADERS: string[] = ["authorization", "user-agent", "content-type"];
   const NON_AUTH_ROUTES: string[] = ["/health"]
@@ -18,9 +34,11 @@ export async function createApp() {
   })
 
   app.addHook("onRequest", async (request, reply) => {
+    let auth = { id: "", email: "" }
     const { url } = request;
-    console.log(url)
-    //if (NON_AUTH_ROUTES.includes(url))
+
+    if (NON_AUTH_ROUTES.includes(url)) return auth
+
   })
   //TODO: 'middleware' de auth pra validar jwt e cookie e retornar o user
 
