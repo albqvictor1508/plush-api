c5fde08c535d8534a2a9d41cff226efa98a1ada8
albqvictor1508 <albq.victor@gmail.com>
2025-09-10 19:00:21 -0300

    style: improve code formatting and adjust date calculation in signup

--- src/functions/auth/signup.ts
+++ src/functions/auth/signup.ts
@@ -8,69 +8,76 @@ import { users } from "src/db/schema/users";
 import type { UserMetadata } from "src/types";
 
 interface SignupOptions {
-	authId: string;
-	email: string;
-	password: string;
-	avatar: string;
-	name: string;
-	meta: UserMetadata;
+  authId: string;
+  email: string;
+  password: string;
+  avatar: string;
+  name: string;
+  meta: UserMetadata;
 }
 
 export const signup = async ({
-	email,
-	avatar,
-	name,
-	password,
-	authId,
-	meta,
+  email,
+  avatar,
+  name,
+  password,
+  authId,
+  meta,
 }: SignupOptions) => {
-	const TWO_MIN_IN_SECS = "120";
-	const { browser, ip, os } = meta;
+  const TWO_MIN_IN_SECS = "120";
+  const { browser, ip, os } = meta;
 
-	const isEmailUsed = await db
-		.select({ email: users.email })
-		.from(users)
-		.where(eq(users.email, email));
-	if (isEmailUsed) throw new Error("tratar erro"); //WARN: tratar erro
+  const isEmailUsed = await db
+    .select({ email: users.email })
+    .from(users)
+    .where(eq(users.email, email));
+  if (isEmailUsed) throw new Error("tratar erro"); //WARN: tratar erro
 
-	const isNameUsed = await db
-		.select({ name: users.name })
-		.from(users)
-		.where(eq(users.name, name));
-	if (isNameUsed) throw new Error("tratar erro"); //WARN: tratar erro
+  const isNameUsed = await db
+    .select({ name: users.name })
+    .from(users)
+    .where(eq(users.name, name));
+  if (isNameUsed) throw new Error("tratar erro"); //WARN: tratar erro
 
-	const id = (await new Snowflake().create()).toString();
-	const [user] = await db
-		.insert(users)
-		.values({
-			id,
-			email,
-			name,
-			password,
-			authId,
-			avatar,
-		})
-		.returning({ id: users.id, email: users.email });
-	if (!user) throw new Error("tratar erro"); //WARN: tratar erro
+  const [user] = await db
+    .insert(users)
+    .values({
+      id: (await new Snowflake().create()).toString(),
+      email,
+      name,
+      password,
+      authId,
+      avatar,
+    })
+    .returning({ id: users.id, email: users.email });
+  if (!user) throw new Error("tratar erro"); //WARN: tratar erro
 
-	await redis.send("SETEX", [
-		`users:${user.id}`,
-		TWO_MIN_IN_SECS,
-		JSON.stringify(user),
-	]);
+  await redis.send("SETEX", [
+    `users:${user.id}`,
+    TWO_MIN_IN_SECS,
+    JSON.stringify(user),
+  ]);
 
-	const access = await generateAccessToken(user);
-	const { hash, token } = generateRefreshToken();
+  const { hash, token } = generateRefreshToken();
+  const access = await generateAccessToken(user);
 
-	const FIFTEEN_MIN_IN_MS = 900000;
-	const FIFTEEN_DAYS_IN_MS = 1.296e9;
+  const FIFTEEN_DAYS_LATER = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000);
 
-	await db.insert(sessions).values({
-		hash,
-		expiresAt: FIFTEEN_DAYS_IN_MS,
-		userId: user.id,
-		browser,
-		ip,
-		os,
-	});
+  await db.insert(sessions).values({
+    hash,
+    expiresAt: FIFTEEN_DAYS_LATER,
+    userId: user.id,
+    browser,
+    ip,
+    os,
+  });
+
+  return {
+    token,
+    access,
+    user: {
+      id: user.id,
+      email: user.email,
+    },
+  };
 };
--- src/modules/sessions/signup.ts
+++ src/modules/sessions/signup.ts
@@ -10,68 +10,66 @@ import { signup } from "src/functions/auth/signup";
 import { parse as getAgent } from "useragent";
 import z from "zod";
 
-const FIFTEEN_DAYS_LATER = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000);
-
 export const route: FastifyPluginAsyncZod = async (app) => {
-	app.post(
-		"/sessions/signup",
-		{
-			schema: {
-				body: z.object({
-					authId: z.optional(z.string()),
-					email: z.email({ error: "Invalid email." }),
-					avatar: z.optional(z.string()),
-					password: z
-						.string()
-						.min(6, {
-							error: "the password must to be greater than 6 characters",
-						})
-						.max(20, {
-							error: "the password must to be less than 20 characters",
-						}), //usar o refine() pra validar senha com caracter especial e td mais
-					name: z
-						.string()
-						.min(3, {
-							error: "the username must to be greater than 3 characters",
-						})
-						.max(16, {
-							error: "the username must to be less than 16 characters",
-						}),
-				}),
-				response: {
-					201: z.void(),
-					400: z.string(),
-					500: z.string(),
-				},
-			},
-		},
-		async (request, reply) => {
-			const agent = getAgent(request.headers["user-agent"]);
-			if (agent.family === "Other") return reply.code(400).send("dps"); //TODO: mudar erro
+  app.post(
+    "/sessions/signup",
+    {
+      schema: {
+        body: z.object({
+          authId: z.optional(z.string()),
+          email: z.email({ error: "Invalid email." }),
+          avatar: z.optional(z.string()),
+          password: z
+            .string()
+            .min(6, {
+              error: "the password must to be greater than 6 characters",
+            })
+            .max(20, {
+              error: "the password must to be less than 20 characters",
+            }), //usar o refine() pra validar senha com caracter especial e td mais
+          name: z
+            .string()
+            .min(3, {
+              error: "the username must to be greater than 3 characters",
+            })
+            .max(16, {
+              error: "the username must to be less than 16 characters",
+            }),
+        }),
+        response: {
+          201: z.void(),
+          400: z.string(),
+          500: z.string(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const agent = getAgent(request.headers["user-agent"]);
+      if (agent.family === "Other") return reply.code(400).send("dps"); //TODO: mudar erro
 
-			const { email, password, avatar, name, authId } = request.body;
-			//salvar avatar no bucket (multipart/data)
-			//validar sessão do usuário
+      const { email, password, avatar, name, authId } = request.body;
+      //salvar avatar no bucket (multipart/data)
+      //validar sessão do usuário
 
-			const {} = await signup({
-				meta: {
-					browser: agent.family,
-					ip: request.ip,
-					os: agent.os.family,
-				},
-				user: {
-					email,
-					name,
-					avatar: avatar ?? "",
-					authId: authId ?? "",
-					password,
-				},
-			});
+      const { } = await signup({
+        meta: {
+          browser: agent.family,
+          ip: request.ip,
+          os: agent.os.family,
+        },
+        user: {
+          email,
+          name,
+          avatar: avatar ?? "",
+          authId: authId ?? "",
+          password,
+        },
+      });
 
-			await reply.setCookie("refresh", token);
-			await reply.setCookie("access", access);
+      await reply.setCookie("refresh", token);
+      await reply.setCookie("access", access);
 
-			return await reply.code(201);
-		},
-	);
+      return await reply.code(201);
+    },
+  );
 };
