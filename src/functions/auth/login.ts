21febb0b4a5c53c447ae2d7ece222c4ee243266a
albqvictor1508 <albq.victor@gmail.com>
2025-09-11 15:59:05 -0300

    refactor: reformat auth login function and update environment configuration

--- src/functions/auth/login.ts
+++ src/functions/auth/login.ts
@@ -1,54 +1,54 @@
 import { eq } from "drizzle-orm";
-import { generateAccessToken, generateRefreshToken } from "src/common/auth";
+import type { UserMetadata } from "src/@types";
+import { generateAccessToken, generateRefreshToken } from "src/config/auth";
 import { db } from "src/db/client";
 import { sessions } from "src/db/schema/sessions";
 import { users } from "src/db/schema/users";
 import { sweepCredentials } from "./sweep-credentials";
-import type { UserMetadata } from "src/types";
 
 interface LoginOptions {
-	email: string;
-	password: string;
-	meta: UserMetadata;
+  email: string;
+  password: string;
+  meta: UserMetadata;
 }
 
 export const login = async ({ email, password, meta }: LoginOptions) => {
-	const [user] = await db
-		.select({ id: users.id, password: users.password })
-		.from(users)
-		.where(eq(users.email, email));
+  const [user] = await db
+    .select({ id: users.id, password: users.password })
+    .from(users)
+    .where(eq(users.email, email));
 
-	if (
-		!user ||
-		(user.password && !(await Bun.password.verify(password, user.password)))
-	)
-		throw new Error("tratar"); //WARN: tratar erro
+  if (
+    !user ||
+    (user.password && !(await Bun.password.verify(password, user.password)))
+  )
+    throw new Error("tratar"); //WARN: tratar erro
 
-	const MAX_SESSIONS_PER_USER = 10;
-	const SEVEN_DAYS_AFTER = new Date(Date.now() + 6.048e8);
+  const MAX_SESSIONS_PER_USER = 10;
+  const SEVEN_DAYS_AFTER = new Date(Date.now() + 6.048e8);
 
-	const { hash, token } = generateRefreshToken();
-	const { browser, ip, os } = meta;
+  const { hash, token } = generateRefreshToken();
+  const { browser, ip, os } = meta;
 
-	const [_, [session]] = await Promise.all([
-		sweepCredentials(user.id, MAX_SESSIONS_PER_USER),
-		db
-			.insert(sessions)
-			.values({
-				userId: user.id,
-				hash,
-				browser,
-				os,
-				ip,
-				expiresAt: SEVEN_DAYS_AFTER,
-			})
-			.returning({ id: sessions.userId }),
-	]);
+  const [_, [session]] = await Promise.all([
+    sweepCredentials(user.id, MAX_SESSIONS_PER_USER),
+    db
+      .insert(sessions)
+      .values({
+        userId: user.id,
+        hash,
+        browser,
+        os,
+        ip,
+        expiresAt: SEVEN_DAYS_AFTER,
+      })
+      .returning({ id: sessions.userId }),
+  ]);
 
-	if (!session) throw new Error("tratar"); //WARN: tratar erro
+  if (!session) throw new Error("tratar"); //WARN: tratar erro
 
-	return {
-		refresh: token,
-		access: await generateAccessToken({ id: session.id, email }),
-	};
+  return {
+    refresh: token,
+    access: await generateAccessToken({ id: session.id, email }),
+  };
 };
