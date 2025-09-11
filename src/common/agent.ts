5b6a8434d92416fedd571ead7a6a5e1f3f26f111
albqvictor1508 <albq.victor@gmail.com>
2025-09-11 10:39:05 -0300

    refactor: use verifyAgent for agent validation and error handling

--- src/common/agent.ts
+++ src/common/agent.ts
@@ -1,8 +1,9 @@
 import { parse as getAgent } from "useragent";
 
-export const verifyAgent = (auth: string) => {
-	const agent = getAgent(auth);
-	if (agent.family === "Other") return "error"; //WARN: tratar erro
+export const verifyAgent = (auth?: string) => {
+  if (!auth) throw new Error("tratar erro"); //WARN: tratar erro
+  const agent = getAgent(auth);
+  if (agent.family === "Other") throw new Error("tratar erro"); //WARN: tratar erro
 
-	return agent;
+  return agent;
 };
