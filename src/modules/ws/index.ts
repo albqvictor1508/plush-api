7abceb82c9a094ab17d7382200ccc29b73c7b79b
albqvictor1508 <albq.victor@gmail.com>
2025-09-17 15:42:17 -0300

    refactor: prepare web socket handler to support event types

--- src/modules/ws/index.ts
+++ src/modules/ws/index.ts
@@ -1,18 +1,26 @@
-import c from "chalk";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import type { EventType } from "src/@types/ws";
+import { EventType } from "src/@types/ws";
 
 interface DataSchema {
-  type: EventType
-  data: {} //esse data tem que mudar para cada evento
+	type: EventType;
+	body: {}; //esse body tem que mudar para cada evento
 }
 
 export const route: FastifyPluginAsyncZod = async (app) => {
-  app.get("/ws", { websocket: true }, async (ws, req) => {
-    ws.on("message", (msg) => {
-      const data = JSON.parse(msg);
-      switch (data.type)
-      ws.send("hello from fastify ws!");
-    });
-  });
+	app.get("/ws", { websocket: true }, async (ws, req) => {
+		ws.on("message", (msg) => {
+			//@ts-expect-error
+			const data: DataSchema = JSON.parse(msg);
+
+			ws.send("hello from fastify ws!");
+			switch (data.type) {
+				case EventType.MESSAGE_CREATED: {
+					return;
+				}
+
+				case EventType.MESSAGE_DELETED: {
+				}
+			}
+		});
+	});
 };
