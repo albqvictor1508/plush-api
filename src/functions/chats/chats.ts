afb88ab97bffefd8bc67916ccb6ccbdb914f2e94
albqvictor1508 <albq.victor@gmail.com>
2025-09-17 11:24:13 -0300

    feat(chats): add function to broadcast messages to chat participants

--- src/functions/chats/chats.ts
+++ src/functions/chats/chats.ts
@@ -10,3 +10,11 @@ export const removeConnection = async (chatId: string, ws: WebSocket) => {
 	if (!chatConnections[chatId]?.has(ws)) throw new Error("error"); //WARN: tratar erro
 	chatConnections[chatId].delete(ws);
 };
+
+export const broadcastToChat = async (chatId: string, message: string) => {
+	if (!chatConnections[chatId]) throw new Error("this chat not exists"); //WARN: tratar erro
+
+	for (const ws of chatConnections[chatId]) {
+		return ws.send(JSON.stringify(message));
+	}
+};
