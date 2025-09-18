cbd019c9c7d6d9eae4d495cd4e96d2d65b7d85ec
albqvictor1508 <albq.victor@gmail.com>
2025-09-18 10:49:57 -0300

    feat(schema): create messages table with chat/user references and message fields

---
+++ src/db/schema/messages.ts
@@ -0,0 +1,19 @@
+import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { chats } from "./chats";
+import { users } from "./users";
+
+export const messages = pgTable("messages", {
+	id: serial().notNull().primaryKey(),
+	chatId: serial("chat_id")
+		.notNull()
+		.references(() => chats.id),
+	userId: text("user_id")
+		.notNull()
+		.references(() => users.id),
+	photo: text(),
+	content: varchar({ length: 255 }).notNull(),
+	sendedAt: timestamp("sended_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at")
+		.notNull()
+		.$onUpdate(() => new Date()),
+});
