8818fa321ecda9c74559c60107953e1416505ac9
albqvictor1508 <albq.victor@gmail.com>
2025-09-18 10:51:07 -0300

    feat(db/schema): add chat participants, chats, and messages models

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,4 +1,7 @@
+import { chatParticipants } from "./chat-participants.ts";
+import { chats } from "./chats.ts";
+import { messages } from "./messages.ts";
 import { sessions } from "./sessions.ts";
 import { users } from "./users.ts";
 
-export const schema = { users, sessions };
+export const schema = { users, sessions, chats, messages, chatParticipants };
