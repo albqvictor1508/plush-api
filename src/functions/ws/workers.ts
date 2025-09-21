d5b0e04f3e13c750ecd977cded1812c1ab172a52
albqvictor1508 <albq.victor@gmail.com>
2025-09-20 23:47:06 -0300

    feat: implement Redis stream consumer for message persistence

--- src/functions/ws/workers.ts
+++ src/functions/ws/workers.ts
@@ -8,19 +8,45 @@ const group = "lume_api_group";
 const consumer = `consumer-${randomUUID()}`;
 
 export const persistMessages = async () => {
-	const data = {
-		id: "",
-		senderId: "",
-		chatId: "",
-		content: "",
-		status: "sended",
-		photo: "",
-		sendedAt: new Date(),
-	};
-	//while (true) {}
+	while (true) {
+		const res = await redis.send("XREADGROUP", [
+			"GROUP",
+			group,
+			consumer,
+			"BLOCK",
+			"2000",
+			"STREAMS",
+			key,
+			">",
+		]);
 
-	await db.insert(messages).values(data);
-	await redis.send("XACK", [`chat:${data.chatId}:messages`, group, data.id]);
+		if (!res) continue;
+
+		const data = {
+			id: "",
+			senderId: "",
+			chatId: "",
+			content: "",
+			status: "sended",
+			photo: "",
+			sendedAt: new Date(),
+		};
+
+		const [stream, messages] = res.shift();
+		const salve = messages.map(([messageId, fields]) => {
+			const msg: {
+				senderId: string;
+				chatId: string;
+				content: string;
+				avatar: string;
+			} = Object.fromEntries(
+				Array.from({ length: fields.length / 2 }, (_, i) => [fields[i * 2]]),
+			);
+		});
+
+		const [persistedMsg] = await db.insert(messages).values(data).returning();
+		await redis.send("XACK", [`chat:${data.chatId}:messages`, group, data.id]);
+	}
 };
 
 export const listenMessages = async () => {};
