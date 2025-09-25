27df37ad0b5e6cc2d6d2b91a592efc6c5aa9b2cb
albqvictor1508 <albq.victor@gmail.com>
2025-09-25 11:45:40 -0300

    feat(chats): add file handling and improved file schema for chat creation

--- src/db/schema/files.ts
+++ src/db/schema/files.ts
@@ -1,13 +1,17 @@
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-
 import { messages } from "./messages";
-export const photos = pgTable("files", {
-  id: text().primaryKey().notNull(),
-  messageId: text()
-    .notNull()
-    .references(() => messages.id),
-  extension: text({
-    enum: ["pdf", "docx", "jpg", "png", "webp", "jpeg"],
-  }).notNull(), //futuramente pode ser um enum
-  sendedAt: timestamp().defaultNow(),
+import { chats } from "./chats";
+import { users } from "./users";
+
+//arquivo pode ser attach, profile, o krl, tenho que ver essa questão
+export const files = pgTable("files", {
+	id: text().primaryKey().notNull(),
+	chatId: text().references(() => chats.id),
+	messageId: text().references(() => messages.id),
+	userId: text().references(() => users.id),
+	extension: text({
+		enum: ["pdf", "docx", "jpg", "png", "webp", "jpeg"],
+	}).notNull(), //futuramente pode ser um enum
+
+	sendedAt: timestamp().defaultNow(),
 });
--- src/modules/chats/create.ts
+++ src/modules/chats/create.ts
@@ -1,13 +1,26 @@
 import type { MultipartFile } from "@fastify/multipart";
+import { eq } from "drizzle-orm";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { EventType } from "src/@types/ws";
-import { getChatAvatar, s3cli } from "src/common/bucket";
+import { getChatMedia, s3cli } from "src/common/bucket";
 import { redis } from "src/common/cache";
 import { Snowflake } from "src/common/snowflake";
 import { STREAM_KEY } from "src/common/ws";
+import { db } from "src/db/client";
+import { files } from "src/db/schema/files";
+import { users } from "src/db/schema/users";
 import { createChat } from "src/functions/chats/create";
+import z from "zod";
 
+type FileExtension = "pdf" | "docx" | "jpg" | "png" | "webp" | "jpeg";
 const fields: Record<string, any> = {};
+
+const createChatSchema = z.object({
+	title: z.string(),
+	ownerId: z.string(),
+	description: z.string(),
+	extension: z.enum(["df"]),
+});
 
 export const route: FastifyPluginAsyncZod = async (app) => {
 	app.post(
@@ -34,16 +47,36 @@ export const route: FastifyPluginAsyncZod = async (app) => {
 
 				file = part;
 			}
-			const id = (await new Snowflake().create()).toString();
+			if (!createChatSchema.safeParse(fields))
+				return reply.code(400).send("Bad Credentials");
+
+			const { title, description, ownerId } = fields;
+
+			const [user] = await db
+				.select({ id: users.id })
+				.from(users)
+				.where(eq(users.id, ownerId));
+			if (!user) return reply.code(404).send("Unknown User");
+
+			const chatId = (await new Snowflake().create()).toString();
+			const fileId = (await new Snowflake().create()).toString();
 			if (file) {
-				path = getChatAvatar(id);
+				path = getChatMedia(chatId, fileId, "profile", file.mimetype);
 				const buffer = await file.toBuffer();
+
+				await db.insert(files).values({
+					id: fileId,
+					extension: file.mimetype as FileExtension,
+					sendedAt: new Date(),
+				});
+
 				s3cli.file(path).write(buffer);
 			}
 
 			const url = s3cli.presign(path);
 
-			const data = { id, avatar: url, ...fields };
+			const data = { chatId, avatar: url, ...fields };
+
 			//@ts-expect-error
 			const res = await createChat(data);
 			if (typeof res === "object" && "error" in res)
